5711bfb82c02f2d45c2913677b2d420912724a6d
Gatus <52807971+Ender420-dev@users.noreply.github.com>
2025-07-16 19:25:47 +0800

    create layout php

---
+++ sidenav.php
@@ -0,0 +1,103 @@
+<div class="sidebar">
+    <nav class="nav flex-column">
+    <a href="#" class="nav-link navbar-brand">
+    <span class="icon"><i class="bi bi-grid"></i></span>
+    <span class=" description">SMS 3</span> 
+    </a>
+
+    <!-- module 1 -->
+    <a href="#" class="nav-link text-center active" data-bs-toggle="collapse" data-bs-target="#module1drop" aria-expanded="false" aria-controls="module1drop">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 1
+        <i class="bi bi-caret-down-fill"></i>
+      </span>
+    </a>
+
+    <!-- submodules 1 -->
+    <div class="sub-menu collapse" id="module1drop">
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 1</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 2</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 3</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 4</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 5</span></a>
+    </div>
+
+    <!-- module 2 -->
+    <a href="#" class="nav-link text-center" data-bs-toggle="collapse" data-bs-target="#module2drop" aria-expanded="false" aria-controls="module2drop">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 2
+        <i class="bi bi-caret-down-fill"></i>
+      </span>
+    </a>
+
+    <!-- submodules 2 -->
+    <div class="sub-menu collapse" id="module2drop">
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 1</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 2</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 3</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 4</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 5</span></a>
+    </div>
+
+    <!-- module 3 -->
+    <a href="#" class="nav-link text-center" data-bs-toggle="collapse" data-bs-target="#module3drop" aria-expanded="false" aria-controls="module3drop">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 3
+        <i class="bi bi-caret-down-fill"></i>
+      </span>
+    </a>
+
+    <!-- submodules 3 -->
+    <div class="sub-menu collapse" id="module3drop">
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 1</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 2</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 3</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 4</span></a>
+      <a href="#" class="nav-link"><i class="bi bi-list-ul"></i> <span class="description">sub module 5</span></a>
+    </div>
+
+    <!-- Repeat similar structure for modules 4 to 10 -->
+    <!-- module 4 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 4</span>
+    </a>
+
+    <!-- module 5 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 5</span>
+    </a>
+
+    <!-- module 6 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 6</span>
+    </a>
+
+    <!-- module 7 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 7</span>
+    </a>
+
+    <!-- module 8 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 8</span>
+    </a>
+
+    <!-- module 9 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 9</span>
+    </a>
+
+    <!-- module 10 -->
+    <a href="#" class="nav-link text-center">
+      <i class="bi bi-bar-chart"></i>
+      <span class="description">Module 10</span>
+    </a>
+    
+  </nav>
+  </div>
