b3bdd1be75502cb8d2b2e41ff1c7d9fc254b18ff
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-07-18 21:51:53 +0800

    Fix: UI

--- landing_page/static/landing_page/js/click-scroll.js
+++ landing_page/static/landing_page/js/click-scroll.js
@@ -1,6 +1,3 @@
-//jquery-click-scroll
-//by syamsul'isul' Arifin
-
 var sectionArray = [1, 2, 3, 4, 5];
 
 $.each(sectionArray, function(index, value){
@@ -35,3 +32,4 @@ $(document).ready(function(){
     $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
     $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
 });
+
