1f143121373bdcf295bd225da52b38e84e44020c
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-07-19 11:12:00 +0800

    Add: empty pages

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import terms_and_conditions, home # landing page
+from .views import register_club, individual_club # view club
+
 
 urlpatterns =[
     path('terms_and_conditions', terms_and_conditions, name='terms_and_conditions'),
     path('home', home, name='home'),
+    path('register_club', register_club, name='register_club'),
+    path('individual_club', individual_club, name='individual_club'),
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -15,3 +15,13 @@ def home(request):
         if request.POST.get('agree') == 'on':
             return render(request, 'landing_page.html')
     return render(request, 'terms.html')
+
+def individual_club(request):
+    if request.method == 'POST':
+        if request.POST.get('action') == 'visit':
+            return render(request, 'individual_club/individual_club.html')
+    return render(request, 'landing_page.html')
+
+def register_club(request):
+    return render(request, 'register/register_club.html')
+
