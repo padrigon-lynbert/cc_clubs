b56ddbd31d4ef9d3b2f947628d60897022e65d5c
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-07-19 15:41:40 +0800

    Fix: configuration for prod auto deploy

--- cc_clubs/settings.py
+++ cc_clubs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6nho(y)e)e0xm##^8mgyd%qh+z@#exel#r5b8oxc)&s#b1-*#b'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECRET_KEY = 'django-insecure-6nho(y)e)e0xm##^8mgyd%qh+z@#exel#r5b8oxc)&s#b1-*#b' --local
+# DEBUG = True --local
+# ALLOWED_HOSTS = [] --local
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
+DEBUG = os.environ.get("DEBUG", True) == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 
 
 # Application definition
