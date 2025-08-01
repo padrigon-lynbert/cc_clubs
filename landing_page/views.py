2e27e21074cb7aa9680243d6b4db4b5048d576f4
SuspiciousApplePie <andreiusita6@gmail.com>
2025-08-01 17:50:21 +0800

    fix: Update post_registration_club function

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Students
+from .models import Students, Clubs
 from django.contrib import messages
-from .forms import RegisterClubForm
+from .forms import ClubRegistrationForm
 from django.db import IntegrityError
 
 # Create your views here.
@@ -74,24 +74,31 @@ def post_registration_club(request):
     
     # Handle user submission
     if request.method == 'POST':
-        form = RegisterClubForm(request.POST)
+        form = ClubRegistrationForm(request.POST)
         if form.is_valid():
-            new_member = form.save(commit=False)
+            club_name = form.cleaned_data['club_name'].strip().lower()
+
+            # Check if the club is already officially registered
+            if Clubs.objects.filter(club_name__iexact=club_name).exists():
+                messages.error(request, 'This club is already registered. Please choose a different name.')
+                return redirect('register_club')
+
+            new_club_registration = form.save(commit=False)
             student_id = request.session.get('member_id')
             # Save user form
             try:
-                student_id = Students.objects.get(id=student_id)
-                new_member.student = student_id
-                new_member.save()
-                messages.success(request, 'Successfully registered in this club.')
+                student = Students.objects.get(id=student_id)
+                new_club_registration.submitted_by = student
+                new_club_registration.save()
+                messages.success(request, 'Successfully submitted a club registration form.')
                 return redirect('register_club')
             except IntegrityError:
-                messages.error(request, 'You are already registered in this club.')
+                messages.error(request, 'The club name is already applied, try a different name.')
                 return redirect('register_club')
         else:
-            messages.error(request, 'Invalid Form.')
+            messages.error(request, 'The club name is already applied, try a different name.')
     else:
-        form = RegisterClubForm()
+        form = ClubRegistrationForm()
     # Render the form
     context = {'form': form}
     return render(request, 'register/register_club.html', context)
