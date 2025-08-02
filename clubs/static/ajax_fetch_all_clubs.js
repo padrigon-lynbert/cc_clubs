d05935270d320552f3b1b90d432e77988b5d7552
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-08-02 23:08:00 +0800

    static-js-import_ready

---
+++ clubs/static/ajax_fetch_all_clubs.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('/clubs/ajax_fetch_all_clubs/')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('clubs-tbody').innerHTML = html;
+    })
+    .catch(err => {
+        console.error('Failed to load clubs:', err);
+    });
+});
