ea7943979698b4583a6ad90fd9b2069473f8dbe0
SuspiciousApplePie <andreiusita6@gmail.com>
2025-08-08 18:30:47 +0800

    feat: Add location and banner field at ClubRegistration form

--- clubs/forms.py
+++ clubs/forms.py
@@ -5,15 +5,22 @@ class ClubRegistrationForm(forms.ModelForm):
     
     class Meta:
         model = ClubApplication
-        fields = ['club_name', 'description']
+        fields = ['banner', 'club_name', 'description', 'location']
         labels = {
+            'banner': 'Club Profile',
             'club_name': 'Name of the club',
-            'description': 'Description'
+            'description': 'Description',
+            'location': 'Club Location',
         }
         widgets = {
+            'banner': forms.FileInput(attrs={
+                'class': 'form-control',
+                'id': 'imageInput',}),
             'club_name': forms.TextInput(attrs={
                 'class': 'form-control', 
                 'maxlength': 255}),
             'description': forms.Textarea(attrs={
+                'class': 'form-control'}),
+            'location': forms.Select(attrs={
                 'class': 'form-control'}),
         }
