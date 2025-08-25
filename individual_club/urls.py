980f4b857627bc9aa25cb5291bb8cda0693850f1
SuspiciousApplePie <andreiusita6@gmail.com>
2025-08-25 14:35:38 +0800

    Update election_club url to include id

--- individual_club/urls.py
+++ individual_club/urls.py
@@ -3,11 +3,13 @@ from .views import individual_club # view club
 from .views import apply_club # no backend yet
 from .views import budget_request # no backend, no acceptance page
 from .views import election_club # only for linking the page
+from .views import get_club_achievement
 
 
 urlpatterns =[
     path('individual_club', individual_club, name='individual_club'),
     path('apply_club', apply_club, name='apply_club'),
     path('budget_request', budget_request, name='budget_request'),
+    path('election_club/<int:club_id>', election_club, name='election_club'),
     path('achievements/<int:club_id>', get_club_achievement, name='club_achievement'),
 ]
