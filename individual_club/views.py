4eceb9fcf69ca56bdc2a0812da0670c8a4eb3264
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-01 23:22:56 +0800

    Import member_application in views

--- individual_club/views.py
+++ individual_club/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.urls import reverse
-from clubs.models import Clubs
+from clubs.models import Clubs, MemberApplication
 from landing_page.models import Users
 
 
