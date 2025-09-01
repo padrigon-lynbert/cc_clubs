f4087e2d77ab012a0820bb0b7636e0478581ed22
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-01 23:16:03 +0800

    Add club_applicants url path

--- individual_club/urls.py
+++ individual_club/urls.py
@@ -3,7 +3,7 @@ from .views import individual_club # view club
 from .views import apply_club # no backend yet
 from .views import budget_request # no backend, no acceptance page
 from .views import election_club # only for linking the page
-from .views import get_club_achievement, dashboard, member_list
+from .views import get_club_achievement, dashboard, member_list, get_club_applicants
 
 urlpatterns =[
     path('individual_club', individual_club, name='individual_club'),
@@ -12,5 +12,6 @@ urlpatterns =[
     path('election_club/<int:club_id>', election_club, name='election_club'),
     path('achievements/<int:club_id>', get_club_achievement, name='club_achievement'),
     path('individual_club/<int:club_id>', dashboard, name='dashboard'),
+    path('club_applicants/<int:club_id>', get_club_applicants, name='club_applicants'),
     path('member_list', member_list, name='member_list'),
 ]
