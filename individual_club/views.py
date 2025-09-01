63d316b4ca42c7c2a4e41b61bfcaa0cb724a66e7
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-01 23:18:33 +0800

    Add user session validator on functions
        - Add validator in election club
        - Add validator in get club achievement
        - Add validator in dashboard

--- individual_club/views.py
+++ individual_club/views.py
@@ -66,17 +66,35 @@ def budget_request(request):
 
 
 def election_club(request, club_id):
+    member_id = request.session.get('member_id')
+    if not member_id:
+        messages.error(request, "You must be logged in to access this page")
+        return redirect(reverse('home') + '#section_3')
+    
+    user = get_object_or_404(Users, id=member_id)
     club = Clubs.objects.get(id=club_id)
     context = {'club': club}
     return render(request, 'election_club.html', context)
 
     
 def get_club_achievement(request, club_id):
+    member_id = request.session.get('member_id')
+    if not member_id:
+        messages.error(request, "You must be logged in to access this page")
+        return redirect(reverse('home') + '#section_3')
+    
+    user = get_object_or_404(Users, id=member_id)
     club = Clubs.objects.get(id=club_id)
     context = {'club': club}
     return render(request, 'club-achievement.html', context)
 
 def dashboard(request, club_id):
+    member_id = request.session.get('member_id')
+    if not member_id:
+        messages.error(request, "You must be logged in to access this page")
+        return redirect(reverse('home') + '#section_3')
+    
+    user = get_object_or_404(Users, id=member_id)
     club = Clubs.objects.get(id=club_id)
     context = {'club': club}
     return render(request, 'individual_club.html', context)
