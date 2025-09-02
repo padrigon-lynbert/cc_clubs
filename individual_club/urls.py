2f217892b3276a8f0ae7ec9010b573c7897a7321
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-02 16:22:17 +0800

    Add trailing slash in urls

--- individual_club/urls.py
+++ individual_club/urls.py
@@ -8,11 +8,11 @@ from .views import get_club_achievement, dashboard, member_list, get_club_applic
 urlpatterns =[
     path('individual_club/', individual_club, name='individual_club'),
     path('individual_club/<int:club_id>/', club_detail, name='club_detail'),
-    path('apply_club', apply_club, name='apply_club'),
-    path('budget_request', budget_request, name='budget_request'),
-    path('election_club/<int:club_id>', election_club, name='election_club'),
-    path('achievements/<int:club_id>', get_club_achievement, name='club_achievement'),
-    path('individual_club/<int:club_id>', dashboard, name='dashboard'),
-    path('club_applicants/<int:club_id>', get_club_applicants, name='club_applicants'),
-    path('member_list', member_list, name='member_list'),
+    path('apply_club/', apply_club, name='apply_club'),
+    path('budget_request/', budget_request, name='budget_request'),
+    path('election_club/<int:club_id>/', election_club, name='election_club'),
+    path('achievements/<int:club_id>/', get_club_achievement, name='club_achievement'),
+    path('individual_club/<int:club_id>/', dashboard, name='dashboard'),
+    path('club_applicants/<int:club_id>/', get_club_applicants, name='club_applicants'),
+    path('member_list/', member_list, name='member_list'),
 ]
