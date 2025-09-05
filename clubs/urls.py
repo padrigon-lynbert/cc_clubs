eca7f9629fc122f889e124c3629c038ef355a3bc
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-05 17:34:20 +0800

    Add url for club_applications_review

--- clubs/urls.py
+++ clubs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_registration_club, ajax_fetch_all_clubs, get_club_details
+from .views import post_registration_club, ajax_fetch_all_clubs, get_club_details, get_club_application
 
 urlpatterns = [
     path('register_club/', post_registration_club, name='register_club'), # register club
     path('ajax_fetch_all_clubs/', ajax_fetch_all_clubs, name='ajax_fetch_all_clubs'), # club repository
     path('clubs/details/<int:club_id>/', get_club_details, name='get_club_details'),
+    path('club-application-review/', get_club_application, name='get_club_application'),
     ]
