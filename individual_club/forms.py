3aa6b6ed884c7d05bb95afc5556f13fc34e78911
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-08 10:57:51 +0800

    Reference MemberApplication in MembershipApplicationForm

--- individual_club/forms.py
+++ individual_club/forms.py
@@ -1,5 +1,9 @@
-from django.forms import ModelForm
+from django.forms import forms
 from django.core.exceptions import ValidationError
 from landing_page.models import Users
 from clubs.models import MemberApplication
 
+class MembershipApplicationForm(forms.ModelForm):
+
+    class Meta:
+        model = MemberApplication
