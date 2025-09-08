f272177a8bd41ff112c36b5bed13ac161a3fbd4f
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-08 12:16:15 +0800

    Add cor size validation in MembershipApplicationForm

--- individual_club/forms.py
+++ individual_club/forms.py
@@ -1,4 +1,4 @@
-from django.forms import forms
+from django import forms
 from django.core.exceptions import ValidationError
 from landing_page.models import Users
 from clubs.models import MemberApplication
@@ -31,4 +31,10 @@ class MembershipApplicationForm(forms.ModelForm):
         self.fields['certificate_of_recognition'].required = True
 
     def clean_certificate_of_recognition(self):
-        self.cleaned_data.get('certificate_of_recognition')
+        cor = self.cleaned_data.get('certificate_of_recognition')
+        mb = 1024 * 1024
+        if cor:
+            if cor.size > 2 * mb:
+                raise ValidationError(f'Your file size {round(cor.size / mb, 2)} MB exceeds 2MB.')
+        
+        return cor
