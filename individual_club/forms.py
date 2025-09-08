3e66c63a440642941309e37bcfcfee5608c11cc9
SuspiciousApplePie <andreiusita6@gmail.com>
2025-09-08 12:03:26 +0800

    Update widgets in MembershipApplicationForm

--- individual_club/forms.py
+++ individual_club/forms.py
@@ -16,3 +16,19 @@ class MembershipApplicationForm(forms.ModelForm):
             'response_text': 'Reason to join club (optional)',
         }
 
+        widgets = {
+            'certificate_of_recognition': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+            }),
+            'response_text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'maxlength': 255,
+            })
+        }
+
+    def __init__(self, *args, **kwargs):
+        super.__init__(*args, **kwargs)
+        self.fields['certificate_of_recognition'].required = True
+
+    def clean_certificate_of_recognition(self):
+        self.cleaned_data.get('certificate_of_recognition')
