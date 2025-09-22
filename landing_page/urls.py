a7ee483603b52451dc9c51114e3fcdca792eca6e
James Gulay <bs.jamesgulay@gmail.com>
2025-09-22 19:03:25 +0800

    create: profile settings ui

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import terms_and_conditions, home # landing page
 from .views import bridge # view club
 from .views import login_from_landing, logout # session
-from .views import global_announcements, global_chat #individual pages with functions
+from .views import global_announcements, profile_settings #individual pages with functions
 
 urlpatterns =[
     path('terms_and_conditions', terms_and_conditions, name='terms_and_conditions'),
@@ -11,5 +11,5 @@ urlpatterns =[
     path('logout', logout, name='logout'),
     path('bridge', bridge, name='bridge'),
     path('global_announcements', global_announcements, name='global_announcements'),
-    path('global_chat', global_chat, name='global_chat'),
+    path('profile_settings', profile_settings, name='profile_settings'),
 ]
