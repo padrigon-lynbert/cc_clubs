78ec42ad155f7c7b3afb8e43280a601b7b933e51
James Gulay <bs.jamesgulay@gmail.com>
2025-10-17 14:52:15 +0800

    modify: set color into bcp color and create new login ui

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -3,6 +3,7 @@ from .views import terms_and_conditions, home # landing page
 # from .views import bridge # view club
 from .views import login_from_landing, logout # session
 from .views import global_announcements, profile_settings #individual pages with functions
+from .views import login_page
 
 urlpatterns = [
     path('terms_and_conditions', terms_and_conditions, name='terms_and_conditions'),
@@ -12,4 +13,5 @@ urlpatterns = [
     # path('bridge', bridge, name='bridge'),
     path('global_announcements', global_announcements, name='global_announcements'),
     path('profile_settings', profile_settings, name='profile_settings'),
+    path('login_page' , login_page, name='login_page')
 ]
